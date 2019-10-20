f2520cb55e102df1bdc88997e0996e25c94df672
Izzy <israelcantu129@gmail.com>
2019-10-20 01:07:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -235,7 +235,7 @@ class Bot {
                 break
             case 242:
                 				bots.botscount++
-				                bots.newname = bots.name + '|' + bots.botscount
+				                bots.newname = bots.name + '|' + mouseX
 				                this.send(buffers.spawn(bots.newname))
                 //this.send(buffers.spawn(bots.name))
                 break
